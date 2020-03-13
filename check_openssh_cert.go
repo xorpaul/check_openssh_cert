df67afdc1d20a20387d9f896e4a8a33e98f9f0a8
Andreas Paul <xorpaul@gmail.com>
2020-03-13 11:30:32 +0100

    add -jsonKey and -receivedCertficiateIsCA parameter

--- check_openssh_cert.go
+++ check_openssh_cert.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,10 +10,10 @@ import (
 	"net/http"
 	"os"
 
-	"golang.org/x/crypto/ssh"
-
+	"github.com/tidwall/gjson"
 	nagios "github.com/xorpaul/go-nagios"
 	H "github.com/xorpaul/gohelper"
+	"golang.org/x/crypto/ssh"
 )
 
 var (
@@ -27,12 +26,7 @@ var (
 	client        *http.Client
 )
 
-type response struct {
-	CertificateAuthority string `json:"ca"`
-	Certificate          string `json:"cert"`
-}
-
-func setupHttpClient() *http.Client {
+func setupHTTPClient() *http.Client {
 	// Get the SystemCertPool, continue with an empty pool on error
 	rootCAs, _ := x509.SystemCertPool()
 	if rootCAs == nil {
@@ -51,7 +45,7 @@ func setupHttpClient() *http.Client {
 	return &http.Client{Transport: tr}
 }
 
-func doRequest(url string) []byte {
+func doRequest(url string) string {
 	H.Debugf("sending request to " + string(url))
 	resp, err := client.Get(url)
 	//H.Debugf("sending HTTP request " + url)
@@ -59,11 +53,12 @@ func doRequest(url string) []byte {
 		H.Fatalf("Error while issuing request to " + url + " Error: " + err.Error())
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	body := string(byteBody)
 	if err != nil {
 		H.Fatalf("Error while reading response body: " + err.Error())
 	}
-	H.Debugf("Received response: " + string(body))
+	H.Debugf("Received response: " + body)
 
 	return body
 }
@@ -72,8 +67,10 @@ func main() {
 	log.SetOutput(os.Stdout)
 
 	var (
-		urlFlag                      = flag.String("url", "https://127.0.0.1/check", "which URL to send the GET to to get the certificate")
+		urlFlag                      = flag.String("url", "https://127.0.0.1:443/check", "which URL to send the GET to to get the certificate")
 		versionFlag                  = flag.Bool("version", false, "show build time and version number")
+		jsonKey                      = flag.String("jsonKey", "certificate", "JSON key which contains the Authorized Key")
+		receivedCertficiateIsCA      = flag.Bool("receivedCertficiateIsCA", false, "the certificate received from the url is a CA, if this is false then a end certificate is expected")
 		verifyAgainstSignatureShaSum = flag.String("verifyAgainstSignatureShaSum", "", "sha256 sum of the received certificate's fingerprint")
 	)
 	flag.BoolVar(&debug, "debug", false, "log debug output, defaults to false")
@@ -97,24 +94,31 @@ func main() {
 		H.Fatalf("Error: Parameter -verifyAgainstSignatureShaSum needs to be set to the SHA256 sum of the recieved certificate's signature, e.g.  SHA256:Ar7bYZIf/BvWbwkFEukZgjK8lRpzvsvbjGbZ9SwWshE")
 	}
 
-	client = setupHttpClient()
+	client = setupHTTPClient()
 	body := doRequest(url)
-	var response response
-	err := json.Unmarshal(body, &response)
-	if err != nil {
-		H.Warnf("Could not parse JSON response: " + string(body) + " Error: " + err.Error())
+	certificateInAuthorizedKeyFormat := gjson.Get(body, *jsonKey).String()
+	if len(certificateInAuthorizedKeyFormat) < 1 {
+		H.Warnf("Could not find specified JSON key: '" + *jsonKey + "' in response: " + body)
 	}
+	//log.Printf(certificateInAuthorizedKeyFormat)
+	//log.Printf("%#v", response)
 	H.Infof("Received valid response from " + url)
 
-	k, _, _, _, err := ssh.ParseAuthorizedKey([]byte(response.Certificate))
+	k, _, _, _, err := ssh.ParseAuthorizedKey([]byte(certificateInAuthorizedKeyFormat))
 	if err != nil {
 		H.Fatalf("Error while parsing OpenSSH authorized key from " + url + " Error: " + err.Error())
 	}
-	cert := k.(*ssh.Certificate)
+	//log.Printf("%#v", k)
+
+	publicKey := k
+	if !*receivedCertficiateIsCA {
+		publicKey = k.(*ssh.Certificate).SignatureKey
+	}
+	//log.Printf(ssh.FingerprintSHA256(k))
 	//log.Printf("%#v", cert)
 
 	nr := nagios.NagiosResult{}
-	if *verifyAgainstSignatureShaSum == ssh.FingerprintSHA256(cert.SignatureKey) {
+	if *verifyAgainstSignatureShaSum == ssh.FingerprintSHA256(publicKey) {
 		nr.ExitCode = 0
 		nr.Text = "Received certificate's signature from " + url + " matched the given sha256 fingerprint " + *verifyAgainstSignatureShaSum
 		nagios.NagiosExit(nr)
